53e6805f68d7a3c81ddd68b47cc500d8b149103d
Shivam <shivam.saxena@eknoldus.com>
2022-01-30 12:08:54 +0530

    Initial Commit

--- src/main/scala/ListLength.scala
+++ src/main/scala/ListLength.scala
@@ -6,16 +6,3 @@ class ListLength{
     if(listInput.isEmpty) accumulator
     else length(listInput.tail, accumulator + 1)
 }
-
-//class ListLength2 {
-//  def length[T](listInput: Iterable[T]): Int = {
-//    @tailrec
-//    def lengthCalculator(listInput: Iterable[T], accumulator: Int = 0): Int =
-//      if(listInput.isEmpty) accumulator
-//      else listInput match {
-//        case listHead :: listTail => lengthCalculator(listTail, accumulator + 1)
-//        // For every head of every tail, until tail == Nil => accumulator will get +1
-//      }
-//    lengthCalculator(listInput)
-//  }
-//}
